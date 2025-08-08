0f4186093da755f955a634f5abc25a8617e9df29
TI Hidalgo <tihidalgo@teleton.org.mx>
2025-08-08 14:55:06 -0600

    Adding database functionality. First revision.

--- backend/datos_carnet/settings.py
+++ backend/datos_carnet/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = "datos_carnet.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "mssql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "1433"),
+        "OPTIONS": {
+            "driver": "ODBC Driver 17 for SQL Server",
+            "extra_params": "TrustServerCertificate=yes;",
+        },
     }
 }
 
