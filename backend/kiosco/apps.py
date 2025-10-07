3398f8e86ee109ab13254072f19f51089133bfb3
TI Hidalgo <tihidalgo@teleton.org.mx>
2025-10-07 10:20:15 -0600

    Fixed apps.py

--- backend/kiosco/apps.py
+++ backend/kiosco/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WhatsappConfig(AppConfig):
+class KioscoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "kiosco"
--- backend/kiosco_crit/settings.py
+++ backend/kiosco_crit/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "kiosco",
+    "kiosco.apps.KioscoConfig",
 ]
 
 # Para desarrollo local
