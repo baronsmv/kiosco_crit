d2e5fbc5925f48c21847f31b26f7452a115da5a5
TI Hidalgo <tihidalgo@teleton.org.mx>
2025-11-28 11:37:25 -0600

    Added Sphinx docs integration and served via nginx container.

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "kiosco_crit"
+copyright = "2025, TI Hidalgo"
+author = "TI Hidalgo"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "es"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "bizstyle"
+html_static_path = ["_static"]
+
+source_suffix = {
+    ".md": "markdown",
+}
+master_doc = "index"
+
+myst_heading_anchors = 3
