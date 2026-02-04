386317717f8db0d6452c54884cd892b094a45ceb
TI Hidalgo <tihidalgo@teleton.org.mx>
2026-02-04 10:38:00 -0600

    Se añade el modelo de query a la vista de administrador.

--- queries/admin.py
+++ queries/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from classes.admin import BaseAdmin
+from .models import Consulta
+
+
+@admin.register(Consulta)
+class ConsultaAdmin(BaseAdmin):
+    list_display = BaseAdmin.list_display
+    list_filter = BaseAdmin.list_filter + ("fecha_consulta",)
+    search_fields = BaseAdmin.search_fields + ("identificador",)
+
+    @admin.display(description="Identificador", ordering="identificador")
+    def mostrar_destino(self, obj):
+        return obj.identificador or "-"
+
+    @admin.display(description="Fecha consulta", ordering="fecha_consulta")
+    def mostrar_fecha_envio_o_consulta(self, obj):
+        return obj.fecha_consulta or "-"
